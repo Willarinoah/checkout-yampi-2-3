fa4571a4473278d5a68405f33fc248330f797c39
User <user@example.com>
2025-01-16 03:40:56 -0300

    env e index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,11 +2,7 @@ import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "@/components/layout/Header";
-import Hero from "@/components/home/Hero";
-import HowItWorks from "@/components/home/HowItWorks";
-import Pricing from "@/components/home/Pricing";
-import ViralSurprises from "@/components/home/ViralSurprises";
-import FAQ from "@/components/home/FAQ";
+
 import Footer from "@/components/layout/Footer";
 
 const Index = () => {
@@ -34,11 +30,6 @@ const Index = () => {
       className="bg-loveblue min-h-screen"
     >
       <Header />
-      <Hero />
-      <HowItWorks />
-      <Pricing />
-      <ViralSurprises />
-      <FAQ />
       <Footer />
     </motion.div>
   );
